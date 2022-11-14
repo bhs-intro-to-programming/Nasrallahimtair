0c95bc5b1837441a72a6112417d84cbe5c0738e3
Nasrallahimtair <113068478+Nasrallahimtair@users.noreply.github.com>
2022-11-14 15:21:32 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -1,8 +1,8 @@
 // For a change of pace, I'm providing you with all the function skeletons. This
 // should save you some time.
 
-const area = (rect) => {
-};
+const area = (rect) => {PlYMKER+=)
+
 
 const higherPaid = (e1, e2) => {
 };
