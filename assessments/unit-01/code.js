fd5a98cf57872262735c24b7b74391e51450dd37
Nasrallahimtair <113068478+Nasrallahimtair@users.noreply.github.com>
2022-12-16 10:55:54 -0800

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -1,5 +1,4 @@
-// You will use this function in one of the problems. You don't need to change
-// it or even worry about how exactly it works.
+const 
 const emit = (a, b, product) => {
   console.log(`${a} times ${b} is ${product}`);
 };
